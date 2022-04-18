dd846fbec6ca42d4f37f31a3b69270feb3d56626
pinto0309 <rmsdh122@yahoo.co.jp>
2022-04-18 23:50:37 +0900

    package

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ubuntu:20.04
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update && apt-get install -y \
+        nano python3-pip python3-mock libpython3-dev \
+        libpython3-all-dev python-is-python3 \
+        software-properties-common nano \
+    && sed -i 's/# set linenumbers/set linenumbers/g' /etc/nanorc \
+    && apt clean \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN pip install -U onnx \
+    && pip install -U onnx-simplifier \
+    && python3 -m pip install -U onnx_graphsurgeon --index-url https://pypi.ngc.nvidia.com \
+    && pip install -U simple_onnx_processing_tools
---
+++ setup.py
@@ -0,0 +1,42 @@
+
+from setuptools import setup, find_packages
+from os import path
+import re
+
+package_name="simple_onnx_processing_tools"
+root_dir = path.abspath(path.dirname(__file__))
+
+with open("README.md") as f:
+    long_description = f.read()
+
+with open(path.join(root_dir, package_name, '__init__.py')) as f:
+    init_text = f.read()
+    version = re.search(r'__version__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+
+setup(
+    name=package_name,
+    version=version,
+    description=\
+        "A set of simple tools for splitting, merging, OP deletion, "+
+        "size compression, rewriting attributes and constants, "+
+        "OP generation, and change opset for ONNX models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Katsuya Hyodo",
+    author_email="rmsdh122@yahoo.co.jp",
+    url="https://github.com/PINTO0309/simple-onnx-processing-tools",
+    license="MIT License",
+    packages=find_packages(),
+    platforms=["linux", "unix"],
+    python_requires=">=3.6",
+    install_requires = [
+        'snc4onnx',
+        'sne4onnx',
+        'snd4onnx',
+        'scs4onnx',
+        'sog4onnx',
+        'sam4onnx',
+        'soc4onnx',
+        'scc4onnx',
+    ],
+)
