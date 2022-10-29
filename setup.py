2eff4c7fc881b7a828ff35f8353515162bdabdfb
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2022-10-29 16:41:50 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'ssc4onnx >= 1.0.4',
         'sio4onnx >= 1.0.2',
         'svs4onnx >= 1.0.0',
-        'onnx2tf >= 1.0.29',
+        'onnx2tf >= 1.0.35',
         'sng4onnx >= 1.0.1',
         'sde4onnx >= 1.0.0',
     ],
