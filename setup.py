9f670f89e7446153261691b51d65effadd8b5d14
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2023-09-20 13:45:06 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -50,13 +50,13 @@ setup(
         'soa4onnx >= 1.0.4',
         'sod4onnx >= 1.0.0',
         'ssi4onnx >= 1.0.2',
-        'ssc4onnx >= 1.0.4',
+        'ssc4onnx >= 1.0.5',
         'sio4onnx >= 1.0.2',
         'svs4onnx >= 1.0.0',
         'onnx2tf >= 1.16.28',
         'sng4onnx >= 1.0.1',
         'sde4onnx >= 1.0.0',
-        'spo4onnx >= 1.0.0',
+        'spo4onnx >= 1.0.1',
     ],
     extras_require = {
         'full': [
